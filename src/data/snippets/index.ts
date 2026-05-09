9cacf9e7305e5d630f1a27f63691cc978235324a
beans <bennywubennywu@gmail.com>
2026-05-09 05:08:40 -0400

    content(snippets): +200 (run 2026-05-09 batch 1/5) [index]

--- src/data/snippets/index.ts
+++ src/data/snippets/index.ts
@@ -15,8 +15,14 @@ import { systemsDesignSnippets } from "./systems-design";
 import { networkingSnippets } from "./networking";
 import { securitySnippets } from "./security";
 import { devopsSnippets } from "./devops";
-import { pythonSnippets20260509 } from "./python-2026-05-09";
-import { csharpSnippets20260509 } from "./csharp-2026-05-09";
+import { pythonSnippets20260509P1 } from "./python-2026-05-09-p1";
+import { pythonSnippets20260509P2 } from "./python-2026-05-09-p2";
+import { pythonSnippets20260509P3 } from "./python-2026-05-09-p3";
+import { pythonSnippets20260509P4 } from "./python-2026-05-09-p4";
+import { csharpSnippets20260509P1 } from "./csharp-2026-05-09-p1";
+import { csharpSnippets20260509P2 } from "./csharp-2026-05-09-p2";
+import { csharpSnippets20260509P3 } from "./csharp-2026-05-09-p3";
+import { csharpSnippets20260509P4 } from "./csharp-2026-05-09-p4";
 
 export type { Snippet, SnippetLanguage } from "./types";
 
@@ -31,6 +37,12 @@ export const snippets = [
   ...networkingSnippets,
   ...securitySnippets,
   ...devopsSnippets,
-  ...pythonSnippets20260509,
-  ...csharpSnippets20260509,
+  ...pythonSnippets20260509P1,
+  ...pythonSnippets20260509P2,
+  ...pythonSnippets20260509P3,
+  ...pythonSnippets20260509P4,
+  ...csharpSnippets20260509P1,
+  ...csharpSnippets20260509P2,
+  ...csharpSnippets20260509P3,
+  ...csharpSnippets20260509P4,
 ];
